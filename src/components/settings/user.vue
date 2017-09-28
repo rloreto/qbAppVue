9e7f2ebfe6a111af065c0da7d1f1e25dd014f312
Roberto Loreto <roberto.loreto@avanade.com>
2017-09-28 18:04:40 +0200

    changes

---
+++ src/components/settings/user.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="window-height window-width bg-light column items-center no-wrap">
+
+  </div>
+</template>
+
+<script>
+/*
+ * Root component
+ */
+export default {}
+</script>
+
+
+<style></style>
